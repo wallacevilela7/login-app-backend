688585f53b31c8635c0329f6ab13178175438cd1
Wallace Vilela <wallace.vilela9@gmail.com>
2024-11-29 15:24:30 -0300

    Create project

---
+++ src/test/java/wvsdev/loginauthapi/LoginAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package wvsdev.loginauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
