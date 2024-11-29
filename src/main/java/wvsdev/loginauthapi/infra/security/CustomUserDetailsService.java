25f296495002fae65520a383744aeef35f6d6a46
Wallace Vilela <wallace.vilela9@gmail.com>
2024-11-29 16:34:06 -0300

    CustomUserDetailsService

---
+++ src/main/java/wvsdev/loginauthapi/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package wvsdev.loginauthapi.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import wvsdev.loginauthapi.domain.user.User;
+import wvsdev.loginauthapi.repositories.UserRepository;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
